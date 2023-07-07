935b27c184c3489c5dc8219addbbea9bc7cf55af
Savvas Michael <savvas.michael@sky.uk>
2023-07-07 11:32:12 +0300

    Removing cleanup for now

--- bootcamp-initialisation/buildCliTool.sh
+++ bootcamp-initialisation/buildCliTool.sh
@@ -8,14 +8,3 @@ echo 'java -cp build/libs/bootcamp-initialize.jar:* io.cecg.bootcamp.initialisat
 
 # Make bootcamp-initialize script executable
 chmod +x bootcamp-initialize
-
-output=$(./bootcamp-initialize --git-token=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 --org=coreeng --modules=p2p-fast-feedback --bootcampee-repo=bootcamp-initialisation-smoke-test)
-echo $output
-
-github_link=$(echo "$output" | grep -o "https://github.com/[[:alnum:]_-]*/[[:alnum:]_-]*/issues/")
-# Extract repository name from the GitHub Link
-repository=$(echo "$github_link" | awk -F'/' '{print $4 "/" $5}')
-
-# GitHub API request to count issues
-issues_count=$(curl -s -H "Authorization: Bearer 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" "https://api.github.com/repos/$repository/issues?state=all" | jq length)
-echo "Number of issues: $issues_count"
