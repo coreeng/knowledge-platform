def2d03524d1e4a40a8d59dde1dfddc1992ac473
Adriana Vasiu <adriana.vasiu2@sky.uk>
2023-09-14 07:45:16 +0100

    Add more test scenarios

--- bootcamp-resources/autograding/multi-tenancy/test/godogs/features/acceptance_criteria.feature
+++ bootcamp-resources/autograding/multi-tenancy/test/godogs/features/acceptance_criteria.feature
@@ -12,28 +12,21 @@ Feature: Multi tenant kubernetes Acceptance Criteria
     Then there is a running "hnc-controller-manager" pod in the namespace "hnc-system"
 
   Scenario Outline: Hierarchical namespaces exist and are created correctly
-    When I get the hierarchical namespaces tree
-    Then the <parent_namespace> namespace has the subnamespaces <subnamespaces>
+    When I get the "<parent-namespace>" namespace
+    Then the namespace "<parent-namespace>" is returned
+    And the "<parent-namespace>" namespace has the subnamespaces "<subnamespaces>"
     Examples:
-      | parent_namespace | subnamespaces                 |
+      | parent-namespace | subnamespaces                 |
       | cecg             | team-a,team-b                 |
       | team-a           | app-1,app-2,team-a-monitoring |
       | team-b           | app-3,team-b-monitoring       |
-#
-#  Scenario Outline: The access to cluster namespaces is isolated per tenant - RBAC rules are applied correctly
-#    Given I have the following namespaces per tenant
-#      | tenant | namespace |
-#      | team-a | team-a    |
-#      | team-b | team-b    |
-#    And the team <team_name> has a service account <team_service_account>
-#    When I impersonate the <team_service_account>
-#    Then I can access the following namespaces: <allowed_namespaces>
-#    And I cannot access the following namespaces: <not_allowed_namespaces>
-#    Examples:
-#      | team_name | team_service_account | allowed_namespaces                   | not_allowed_namespaces               |
-#      | team-a    | team-a-sa            | team-a,app-1,app-2,team-a-monitoring | team-b,app-3,team-b-monitoring       |
-#      | team-b    | team-b-sa            | team-b,app-3,team-b-monitoring       | team-a,app-1,app-2,team-a-monitoring |
-#
-#
-#
 
+  Scenario Outline: The access to cluster namespaces is isolated per tenant - RBAC rules are applied correctly
+    Given a service account for "<team-name>" already exists
+    When I impersonate the service account
+    Then I can access the following namespaces: "<allowed_namespaces>"
+    And I cannot access the following namespaces: "<not_allowed_namespaces>"
+    Examples:
+      | team-name | allowed_namespaces                   | not_allowed_namespaces               |
+      | team-a    | team-a,app-1,app-2,team-a-monitoring | team-b,app-3,team-b-monitoring       |
+      | team-b    | team-b,app-3,team-b-monitoring       | team-a,app-1,app-2,team-a-monitoring |
--- bootcamp-resources/autograding/multi-tenancy/test/godogs/functional_test.go
+++ bootcamp-resources/autograding/multi-tenancy/test/godogs/functional_test.go
@@ -10,25 +10,26 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"sigs.k8s.io/hierarchical-namespaces/api/v1alpha2"
 	"strings"
 	"testing"
 )
 
-type multiTenancyFeature struct {
-	kubernetesClient *kubernetes.Clientset
-	namespaces       map[string]*corev1.Namespace
-	podPerNamespace  map[string]*corev1.PodList
-}
+var kubernetesClient *kubernetes.Clientset
+var impersonateAccountClient *kubernetes.Clientset
+var accountToImpersonate string
+var namespaces map[string]*corev1.Namespace
+var podPerNamespace map[string]*corev1.PodList
 
-func (feature *multiTenancyFeature) theKubernetesClientIsSetUp() error {
-	if feature.kubernetesClient == nil {
+func theKubernetesClientIsSetUp() error {
+	if kubernetesClient == nil {
 		logrus.Info("Attempting to fetch in-cluster config..")
 		config, err := rest.InClusterConfig()
 		if err != nil {
 			return fmt.Errorf("failed fetching in cluster config, err: %v", err)
 		}
 		logrus.Info("Attempting to create a kubernetes client..")
-		feature.kubernetesClient, err = kubernetes.NewForConfig(config)
+		kubernetesClient, err = kubernetes.NewForConfig(config)
 		if err != nil {
 			return fmt.Errorf("failed creating a kubernetes client, err: %v", err)
 		}
@@ -38,8 +39,8 @@ func (feature *multiTenancyFeature) theKubernetesClientIsSetUp() error {
 	return nil
 }
 
-func (feature *multiTenancyFeature) iGetTheNamespace(namespaceName string) error {
-	namespace, err := feature.kubernetesClient.CoreV1().Namespaces().Get(context.TODO(), namespaceName, metav1.GetOptions{})
+func iGetTheNamespace(namespaceName string) error {
+	namespace, err := kubernetesClient.CoreV1().Namespaces().Get(context.TODO(), namespaceName, metav1.GetOptions{})
 	if err != nil {
 		if errors.IsNotFound(err) {
 			// Not found -- handled in the subsequent step
@@ -47,38 +48,38 @@ func (feature *multiTenancyFeature) iGetTheNamespace(namespaceName string) error
 		}
 		return fmt.Errorf("error occurred while fetching the %s namespace", namespaceName)
 	}
-	if feature.namespaces == nil {
-		feature.namespaces = make(map[string]*corev1.Namespace)
+	if namespaces == nil {
+		namespaces = make(map[string]*corev1.Namespace)
 	}
-	feature.namespaces[namespaceName] = namespace
+	namespaces[namespaceName] = namespace
 	return nil
 }
 
-func (feature *multiTenancyFeature) theNamespaceIsReturned(namespaceName string) error {
-	if feature.namespaces[namespaceName] == nil {
+func theNamespaceIsReturned(namespaceName string) error {
+	if namespaces[namespaceName] == nil {
 		return fmt.Errorf("%s namespace not yet set up", namespaceName)
 	}
 	return nil
 }
 
-func (feature *multiTenancyFeature) iGetThePodsInTheNamespace(namespace string) error {
-	pods, err := feature.kubernetesClient.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+func iGetThePodsInTheNamespace(namespace string) error {
+	pods, err := kubernetesClient.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		return fmt.Errorf("error while listing pods in the %s namespace", namespace)
 	}
-	if feature.podPerNamespace == nil {
-		feature.podPerNamespace = make(map[string]*corev1.PodList)
+	if podPerNamespace == nil {
+		podPerNamespace = make(map[string]*corev1.PodList)
 	}
-	feature.podPerNamespace[namespace] = pods
+	podPerNamespace[namespace] = pods
 	return nil
 }
 
-func (feature *multiTenancyFeature) thereIsARunningPod(podName string, namespace string) error {
-	if len(feature.podPerNamespace[namespace].Items) == 0 {
+func thereIsARunningPod(podName string, namespace string) error {
+	if len(podPerNamespace[namespace].Items) == 0 {
 		return fmt.Errorf("no pods are running in the %s namespace", namespace)
 	}
 
-	for _, pod := range feature.podPerNamespace[namespace].Items {
+	for _, pod := range podPerNamespace[namespace].Items {
 		if strings.Contains(pod.Name, podName) {
 			return nil
 		}
@@ -86,13 +87,79 @@ func (feature *multiTenancyFeature) thereIsARunningPod(podName string, namespace
 	return fmt.Errorf("the pod %s is not running in the namespace %s", podName, namespace)
 }
 
+func theNamespaceHasTheSubnamespaces(namespaceName string, subnamespacesNames string) error {
+	// at this point we know that the namespace already exists
+	for _, nsName := range strings.Split(subnamespacesNames, ",") {
+		childNamespace, err := kubernetesClient.CoreV1().Namespaces().Get(context.TODO(), nsName, metav1.GetOptions{})
+		if err != nil {
+			if errors.IsNotFound(err) {
+				return fmt.Errorf("namespae %s is not found", nsName)
+			}
+			return fmt.Errorf("error occurred while fetching the %s namespace", nsName)
+		}
+		subnamespaceAnnotation := childNamespace.Annotations[v1alpha2.SubnamespaceOf]
+		if subnamespaceAnnotation != namespaceName {
+			return fmt.Errorf("the namespace %s is not setup correctly", nsName)
+		}
+	}
+	return nil
+
+}
+
+func iCanAccessTheFollowingNamespaces(namespaces string) error {
+	//TODO - add namespace here from the input
+	pods, err := impersonateAccountClient.CoreV1().Pods("team-a").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		println("----Error while fetching pods----")
+		println(err.Error())
+		return fmt.Errorf("could not fetch namespaces")
+	}
+	for _, pod := range pods.Items {
+		println("***Pods***")
+		println(pod.Name)
+	}
+	return nil
+}
+
+func iCannotAccessTheFollowingNamespaces(arg1 string) error {
+	return nil
+}
+
+func aServiceAccountForNamespaceAlreadyExists(arg1 string) error {
+	return nil
+}
+
+func iImpersonateTheServiceAccount() error {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return fmt.Errorf("failed fetching in cluster config, err: %v", err)
+	}
+
+	//TODO - add account to impersonate from input
+	accountToImpersonate = fmt.Sprintf("system:serviceaccount:%s", "team-a:team-a-sa")
+	println("---Account to impersonate")
+	println(accountToImpersonate)
+	config.Impersonate = rest.ImpersonationConfig{UserName: accountToImpersonate}
+	logrus.Info("Attempting to create a kubernetes client after impersonation..")
+	impersonateAccountClient, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		return fmt.Errorf("error occured while impersonating service account")
+	}
+	logrus.Info("Impersonation was successful..")
+	return nil
+}
+
 func InitializeScenario(ctx *godog.ScenarioContext) {
-	feature := &multiTenancyFeature{}
-	ctx.Step(`^I get the "([^"]*)" namespace$`, feature.iGetTheNamespace)
-	ctx.Step(`^the kubernetes client is setup$`, feature.theKubernetesClientIsSetUp)
-	ctx.Step(`^the namespace "([^"]*)" is returned$`, feature.theNamespaceIsReturned)
-	ctx.Step(`^I get the pods in the "([^"]*)" namespace$`, feature.iGetThePodsInTheNamespace)
-	ctx.Step(`^there is a running "([^"]*)" pod in the namespace "([^"]*)"$`, feature.thereIsARunningPod)
+	ctx.Step(`^I get the "([^"]*)" namespace$`, iGetTheNamespace)
+	ctx.Step(`^the kubernetes client is setup$`, theKubernetesClientIsSetUp)
+	ctx.Step(`^the namespace "([^"]*)" is returned$`, theNamespaceIsReturned)
+	ctx.Step(`^I get the pods in the "([^"]*)" namespace$`, iGetThePodsInTheNamespace)
+	ctx.Step(`^there is a running "([^"]*)" pod in the namespace "([^"]*)"$`, thereIsARunningPod)
+	ctx.Step(`^the "([^"]*)" namespace has the subnamespaces "([^"]*)"$`, theNamespaceHasTheSubnamespaces)
+	ctx.Step(`^I can access the following namespaces: "([^"]*)"$`, iCanAccessTheFollowingNamespaces)
+	ctx.Step(`^I cannot access the following namespaces: "([^"]*)"$`, iCannotAccessTheFollowingNamespaces)
+	ctx.Step(`^a service account for "([^"]*)" already exists$`, aServiceAccountForNamespaceAlreadyExists)
+	ctx.Step(`^I impersonate the service account$`, iImpersonateTheServiceAccount)
 }
 
 func TestFeatures(t *testing.T) {
